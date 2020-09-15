304bedb12218afdf5a588122b076e6d3c6cfe3fd
Aleksandr3012 <flaming541@gmail.com>
2020-09-15 17:02:38 +0300

    commit

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
-1. Отключил lazy плагин. Поставил  loading="lazy"
+<!-- <https://github.com/Aleksandr3012/dentistry> -->
+1. <https://Aleksandr3012.github.io/dentistry/public>
--- sourse/js/common.js
+++ sourse/js/common.js
@@ -132,14 +132,6 @@ const JSCCommon = {
 	},
 	// /табы
 
-	inputMask() {
-		// mask for input
-		let InputTel = [].slice.call(document.querySelectorAll('input[type="tel"]'));
-		InputTel.forEach(function (element) {
-			element.setAttribute("pattern", "[+][0-9]{1}[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}")
-		});
-		Inputmask("+9(999)999-99-99").mask(InputTel);
-	},
 	// /inputMask
 	ifie() {
 		var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
